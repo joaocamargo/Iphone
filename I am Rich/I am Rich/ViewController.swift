18025cc33278b56b62fd7a9788ea7f5c3f663d70
Joao Camargo <joaocamargo@Joaos-MacBook-Pro-10.local>
2019-12-22 10:59:21 -0300

    meu primeiro app

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Joao Camargo on 22/12/19.
+//  Copyright © 2019 Joao Camargo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
