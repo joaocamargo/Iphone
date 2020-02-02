353f4521f6ab68a8c380e97b03fbaa5efa079c64
joao camargo <joaocamargo@joaos-MacBook-Pro.local>
2020-02-02 15:30:25 -0300

    section 10

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft":3,"Medium":4,"Hard":7]
+    //var secondsRemaining = 60
+    
+   
+    var totalTime=0
+    var secondsPassed=0
+    
+    var timer = Timer()
+    
     let softTime = "5"
     let mediumTime = "8"
     let hardTime = "12"
@@ -19,10 +30,18 @@ class ViewController: UIViewController {
 
     @IBAction func hardnesSelected(_ sender: UIButton) {
         print(sender.currentTitle!)
+        progressBar.progress=0
+        secondsPassed=0
+
+        //progressBar.progress = 1.0
 
         let hardness = sender.currentTitle!
+        lblResultado.text=hardness
+        let initialText = "How do you like your eggs?"
         
-        var initialText = "How do you like your eggs?"
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
         //lblResultado.text = lblResultado.text! + "\n"+hardness
         
@@ -43,6 +62,21 @@ class ViewController: UIViewController {
 
         }
         
+    }
+    
+    @objc func updateTimer()
+    {
+        if secondsPassed<totalTime {
+            let percentProgress = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = percentProgress
+            print(percentProgress)
+            secondsPassed+=1
+        }
+        else{
+            timer.invalidate()
+            lblResultado.text = "Done!"
+            progressBar.progress = 1
+        }
     }
     
 }
